0423df54397f43ee696d1a993328378643c9365b
BOTSA-CHANIKYA <119731770+BOTSA-CHANIKYA@users.noreply.github.com>
2022-12-10 14:10:12 +0530

    #include<stdio.h> int main() {     int a,sum=0,i;     scanf("%d",&a);     int n[a];     for(i=0;i<a;i++)     {         scanf("%d",&n[i]);     }     for(i=0;i<a;i++)     {         if(i%2==0)         {             sum=sum+n[i];         }     }     printf("%d",sum); }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,sum=0,i;
+    scanf("%d",&a);
+    int n[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&n[i]);
+    }
+    for(i=0;i<a;i++)
+    {
+        if(i%2==0)
+        {
+            sum=sum+n[i];
+        }
+    }
+    printf("%d",sum);
+}
