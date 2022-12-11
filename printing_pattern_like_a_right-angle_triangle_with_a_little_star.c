643a37948f4a30d2313059d2ae495cd57ec7ae89
BOTSA-CHANIKYA <119731770+BOTSA-CHANIKYA@users.noreply.github.com>
2022-12-11 08:32:43 +0530

    #include<stdio.h> int main() {     int a,s,i,d;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         for(s=1;s<=i;s++)         {             printf("*");         }         {             printf(" ");         }     } }

---
+++ printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,s,i,d;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        for(s=1;s<=i;s++)
+        {
+            printf("*");
+        }
+        {
+            printf("
+");
+        }
+    }
+}
