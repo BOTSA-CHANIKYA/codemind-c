abdf219de3087fefc5afccff37db655ae93febb8
BOTSA-CHANIKYA <119731770+BOTSA-CHANIKYA@users.noreply.github.com>
2022-12-16 17:30:51 +0530

    #include<stdio.h> int main() {     char n;     scanf("%c",&n);     if(n=='a')     {         printf("VOWEL");     }     else if(n=='e')     {         printf("VOWEL");     }     else if(n=='i')     {         printf("VOWEL");     }     else if(n=='o')     {         printf("VOWEL");     }     else if(n=='u')     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    char n;
+    scanf("%c",&n);
+    if(n=='a')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='e')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='i')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='o')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='u')
+    {
+        printf("VOWEL");
+    }
+    else 
+    {
+        printf("CONSONANT");
+    }
+}
