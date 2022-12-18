f3787b8161dbe1f5564183d76ef675d569b33788
BOTSA-CHANIKYA <119731770+BOTSA-CHANIKYA@users.noreply.github.com>
2022-12-18 10:03:42 +0530

    #include<stdio.h> int  main() { 	 int i,n,s; 	scanf("%d",&n); 	 int a[n];     for(i=0;i<n;i++) 	{ 	  scanf("%d",&a[i]); 	} 	for(i=0;i<n;i++) 	{ 	 if(a[i]==0||a[i]==1) 	 s=0; 	 else 	 s=1; 	} 	if(s==0) 	printf("True"); 	else 	printf("False"); }

---
+++ Binary_Array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int  main()
+{	
+	 int i,n,s;
+	scanf("%d",&n);
+	 int a[n];
+    for(i=0;i<n;i++)
+	{ 
+	  scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	 if(a[i]==0||a[i]==1)
+	 s=0;
+	 else
+	 s=1;
+	}
+	if(s==0)
+	printf("True");
+	else
+	printf("False");
+}
