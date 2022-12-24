361893ca219268084ee6db99897c228f964b8d0b
BOTSA-CHANIKYA <119731770+BOTSA-CHANIKYA@users.noreply.github.com>
2022-12-24 22:52:27 +0530

    #include<stdio.h> int main() {     int a,b,c,i,n;     scanf("%d",&n);     a=0;     b=1;     printf("0 1 ");     for(i=2;i<n;i++)     {         c=a+b;         printf("%d ",c);         a=b;         b=c;     } }

---
+++ fibonacci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,i,n;
+    scanf("%d",&n);
+    a=0;
+    b=1;
+    printf("0 1 ");
+    for(i=2;i<n;i++)
+    {
+        c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+    }
+}
