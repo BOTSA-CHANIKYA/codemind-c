7c3a07ad4db5593587040310d6bd66882e4fc974
BOTSA-CHANIKYA <119731770+BOTSA-CHANIKYA@users.noreply.github.com>
2023-01-11 11:25:32 +0530

    #include<stdio.h> int main() {     int a,i,j,c=0;     scanf("%d",&a);     int n[a];     for(i=0;i<a;i++)     {         scanf("%d",&n[i]);     }     for(i=0;i<a;i++)     {         c=0;         for(j=0;j<a;j++)         {             if(n[i]==n[j])             {                 c++;             }         }         if(c==1)         {             printf("%d",n[i]);         }     } }

---
+++ Single_Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j,c=0;
+    scanf("%d",&a);
+    int n[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&n[i]);
+    }
+    for(i=0;i<a;i++)
+    {
+        c=0;
+        for(j=0;j<a;j++)
+        {
+            if(n[i]==n[j])
+            {
+                c++;
+            }
+        }
+        if(c==1)
+        {
+            printf("%d",n[i]);
+        }
+    }
+}
